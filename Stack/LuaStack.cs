a4c125b1ae26b78771c616bd759d009e00272ae8
SuperLoserPeco <mathjohnfh03@163.com>
2019-02-06 18:02:08 +0800

    stack

--- Stack/LuaStack.cs
+++ Stack/LuaStack.cs
@@ -5,11 +5,24 @@ namespace Stack
 {
     public class LuaStack
     {
+        const int MAX_OBJECT_NUM = 10000;
+
         private List<object> slots = new List<object>();
 
         public int Top()
         {
             return slots.Count;
         }
+
+        public void Push(object val)
+        {
+            if(slots.Count > MAX_OBJECT_NUM)
+            {
+                return;
+            }
+            slots.Add(val);
+        }
+
+
     }
 }
