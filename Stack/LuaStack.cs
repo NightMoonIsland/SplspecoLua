5e633f8a02a8f746176048b4b19c02f4c7612e24
SPLS_PECO <mathjohnfh03@163.com>
2019-02-06 17:56:07 +0800

    stack prepare

---
+++ Stack/LuaStack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class LuaStack
+    {
+        private List<object> slots = new List<object>();
+
+        public int Top()
+        {
+            return slots.Count;
+        }
+    }
+}
---
+++ Stack/LuaValueHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class LuaValueHandler
+    {
+        public static LuaValueTypeEnum Typeof(Object val)
+        {
+            if(val == null)
+            {
+                return LuaValueTypeEnum.LUA_TNIL;
+            }
+            else if(val.GetType() == typeof(bool))
+            {
+                return LuaValueTypeEnum.LUA_TBOOLEAN;
+            }
+            else if(val.GetType() == typeof(long) || val.GetType() == typeof(double))
+            {
+                return LuaValueTypeEnum.LUA_TNUMBER;
+            }
+            else if(val.GetType() == typeof(string))
+            {
+                return LuaValueTypeEnum.LUA_TSTRING;
+            }
+            else
+            {
+                return LuaValueTypeEnum.LUA_TNIL;
+            }
+        }
+    }
+}
