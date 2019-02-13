410b03b712050476285b57a5854a46a6e37b9987
SuperLoserPeco <mathjohnfh03@163.com>
2019-02-13 10:54:49 +0800

    State

--- State/LuaStack.cs
+++ State/LuaStack.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Stack
+namespace State
 {
     public class LuaStack
     {
--- State/LuaValueHandler.cs
+++ State/LuaValueHandler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Stack
+namespace State
 {
     public class LuaValueHandler
     {
