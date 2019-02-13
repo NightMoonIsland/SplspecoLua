926a28c12ddbf7549693fcd4f0c7e148a5955480
SuperLoserPeco <3323958613@qq.com>
2019-02-13 15:36:06 +0800

    Interface

---
+++ Api/Interface/ILuaVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api
+{
+    public interface ILuaVM : ILuaState
+    {
+        int GetPC();
+        void AddPC(int n);
+        int Fetch();
+        void GetConst(int idx);
+        void GetRK(int rk);
+    }
+}
---
+++ Vm/InstructionsImplement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Api;
+
+namespace Vm
+{
+    public class InstructionsImplement
+    {
+        public static void Move(int i, ILuaVM vm)
+        {
+            int a = Instruction.getA(i);
+            int b = Instruction.getB(i);
+            vm.Copy(b, a);
+        }
+
+        public static void Jmp(int i, ILuaVM vm)
+        {
+            int a = Instruction.getA(i);
+            int sBx = Instruction.getSBx(i);
+            vm.AddPC(sBx);
+            if(a != 0)
+            {
+                throw new Exception("Jmp: a != 0");
+            }
+        }
+
+        public static void LoadNil(int i, ILuaVM vm)
+        {
+
+        }
+    }
+}
