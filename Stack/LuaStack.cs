f9f4f6c245f5c12c94dea20abb1cce9ce9423f8f
SuperLoserPeco <mathjohnfh03@163.com>
2019-02-13 10:49:25 +0800

    LuaStack

--- Stack/LuaStack.cs
+++ Stack/LuaStack.cs
@@ -23,6 +23,47 @@ namespace Stack
             slots.Add(val);
         }
 
+        public Object Pop()
+        {
+            int _count = slots.Count;
+            Object ret = slots[_count - 1];
+            slots.RemoveAt(_count - 1);
+            return ret;
+        }
 
+        public int AbsIndex(int idx)
+        {
+            return idx >= 0 ? idx : idx + slots.Count + 1;
+        }
+
+        public bool IsValid(int idx)
+        {
+            int absIdx = AbsIndex(idx);
+            return absIdx > 0 && absIdx <= slots.Count;
+        }
+
+        public Object Get(int idx)
+        {
+            int absIdx = AbsIndex(idx);
+            if(absIdx > 0 && absIdx <= slots.Count)
+            {
+                return slots[absIdx - 1];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Set(int idx, Object obj)
+        {
+            int absIdx = AbsIndex(idx);
+            slots.Insert(absIdx - 1, obj);
+        }
+
+        public void Reverse(int from, int to)
+        {
+            slots.Reverse(from, to - from + 1);
+        }
     }
 }
