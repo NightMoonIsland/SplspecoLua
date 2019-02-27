dda14d6dd1d4e88ada83a532c8465bf00bc3be80
SuperLoserPeco <3323958613@qq.com>
2019-02-27 14:30:13 +0800

    code comment

--- Api/Enum/OpArgMaskEnum.cs
+++ Api/Enum/OpArgMaskEnum.cs
@@ -6,9 +6,26 @@ namespace Api
 {
     public enum OpArgMaskEnum
     {
+        /// <summary>
+        /// 不表示任何信息 不会被使用
+        /// </summary>
         OpArgN,
+
+        /// <summary>
+        /// 布尔值 整数值 upvalue索引 子函数索引
+        /// </summary>
         OpArgU,
+
+        /// <summary>
+        /// iABC模式下 表示寄存器索引
+        /// iAsBx模式下 表示跳转偏移
+        /// </summary>
         OpArgR,
+
+        /// <summary>
+        /// 常量表索引 
+        /// 寄存器索引
+        /// </summary>
         OpArgK,
     }
 }
